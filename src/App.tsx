818255c8494724b0d7012d2bf91f96907a22ff81
Davide Baldi <d.baldi88@gmail.com>
2024-08-28 17:37:26 +0200

    fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
 
+import { useEffect } from 'react';
 
+
+
+
+
+const getData = async()=> {
+  try {
+    const response= await fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a495081560f05b8b616be3bb640a8954");
+    const data= await response.json();
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
 
 function App() {
   
+useEffect(()=> {
+  getData()
+},[])
+
+
 
   return (
     <>
       <h1>testing</h1>
+      
     </>
   )
 }
